5eb14e862232fccc29dba76bf3dcba5c1fd838e7
Robert Domingo <robertdomingo@protonmail.com>
2021-04-23 16:06:00 -0400

    Add functional version of reduce

--- functional.js
+++ functional.js
@@ -1,59 +1,99 @@
 let nStates = ["Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota"];
 
-// Returns a URL-friendly verison of a string
-// Example: "North Dakota" -> "north-dakota"
-function urlify(string) {
-  return string.toLowerCase().split(/\s+/).join("-")
-}
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// urls: Imperative version
-function imperativeUrls(elements) {
-  let urls = [];
-  elements.forEach(function(element) {
-    urls.push(urlify(element));
+// sum: Imperative solution
+function imperativeSum(array){
+  let total = 0;
+  array.forEach(function(element) {
+    total += element;
   });
-  return urls;
+  return total;
 }
-console.log(imperativeUrls(nStates));
+console.log(imperativeSum(numbers));
 
-// urls: Functional version
-function functionalUrls(elements) {
-  return elements.map(element => urlify(element));
+// sum: Functional solution
+function functionalSum(elements){
+  return elements.reduce((total, n) => { return total += n; });
 }
-console.log(functionalUrls(nStates));
+console.log(functionalSum(numbers));
 
-/// Using map, write a function that takes in the states variable and returns an array of URLs of the form https://example.com/<urlified form>.
+lengths: Imperative solution
 
-function toUrl(elements) {
-  return elements.map(element => `https://example.com/${urlify(element)}`);
-}
-
-console.log(toUrl(nStates));
-
-Filter - Imperatcie
-
-function imperativeOneWordStates(elements) {
-  let oneWordStrings = [];
+function imperativeLengths(elements){
+  let lengths = {};
   elements.forEach(function(element) {
-    if (element.split(/\s+/).length === 1) {
-      oneWordStrings.push(element)
-    }
-  })
-  return oneWordStrings;
+    lengths[element] = element.length;
+    // console.log(lengths);
+  });
+  return lengths;
 }
-console.log(imperativeOneWordStates(nStates));
+console.log(imperativeLengths(nStates));
 
-function functionalOneWordStates(elements) {
-  return elements.filter(element => element.split(/\s+/).length === 1);
-}
-console.log(functionalOneWordStates(nStates));
+lengths: Functional solution
 
-function northStates(elements) {
-  return elements.filter(element => element.includes("North"));
+function functionalLengths(elements){
+  return elements.reduce((lengths, element) => {
+                          lengths[element] = element.length;
+                          return lengths;
+                        }, {});
 }
-console.log(northStates(nStates));
+console.log(functionalLengths(nStates));
 
-function twoWordStates(elements) {
-  return elements.filter(element => element.split(/\s+/).length === 2);
-}
-console.log(twoWordStates(nStates));
+// // Returns a URL-friendly verison of a string
+// // Example: "North Dakota" -> "north-dakota"
+// function urlify(string) {
+//   return string.toLowerCase().split(/\s+/).join("-")
+// }
+
+// // urls: Imperative version
+// function imperativeUrls(elements) {
+//   let urls = [];
+//   elements.forEach(function(element) {
+//     urls.push(urlify(element));
+//   });
+//   return urls;
+// }
+// console.log(imperativeUrls(nStates));
+
+// // urls: Functional version
+// function functionalUrls(elements) {
+//   return elements.map(element => urlify(element));
+// }
+// console.log(functionalUrls(nStates));
+
+// /// Using map, write a function that takes in the states variable and returns an array of URLs of the form https://example.com/<urlified form>.
+
+// function toUrl(elements) {
+//   return elements.map(element => `https://example.com/${urlify(element)}`);
+// }
+
+// console.log(toUrl(nStates));
+
+// Filter - Imperatcie
+
+// function imperativeOneWordStates(elements) {
+//   let oneWordStrings = [];
+//   elements.forEach(function(element) {
+//     if (element.split(/\s+/).length === 1) {
+//       oneWordStrings.push(element)
+//     }
+//   })
+//   return oneWordStrings;
+// }
+// console.log(imperativeOneWordStates(nStates));
+
+// function functionalOneWordStates(elements) {
+//   return elements.filter(element => element.split(/\s+/).length === 1);
+// }
+// console.log(functionalOneWordStates(nStates));
+
+// function northStates(elements) {
+//   return elements.filter(element => element.includes("North"));
+// }
+// console.log(northStates(nStates));
+
+// function twoWordStates(elements) {
+//   return elements.filter(element => element.split(/\s+/).length === 2);
+// }
+// console.log(twoWordStates(nStates));
