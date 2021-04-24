4813fe61119a3f80ee4821bf8e2d40f412ec7078
Robert Domingo <robertdomingo@protonmail.com>
2021-04-24 17:05:04 -0400

    Define Phrase object with palindrome method

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,19 @@
 // Reverses a string.
 function reverse(string) {
-  return Array.from(string) .reverse().join("");
+  return Array.from(string).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string){
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    let processedContent = this.content.toLowerCase();
+    return processedContent === reverse(processedContent);
+  }
+
+  this.louder = function() {
+    return this.content.toUpperCase();
+  }
 }
