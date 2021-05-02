1ddb0308166b2673070d91006dfe3577b514bd8e
Robert Domingo <robertdomingo@protonmail.com>
2021-05-02 19:03:17 -0400

    Update Phrase and TranslatedPhrase constructors

--- palindrome.js
+++ palindrome.js
@@ -7,13 +7,30 @@ function reverse(string) {
 function Phrase(content) {
   this.content = content;
 
-  // Returns true for a palindrome, false otherwise.
-  this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+  this.processor = function(string) {
+    return string.toLowerCase();
   }
 
-  this.louder = function() {
-    return this.content.toUpperCase();
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
   }
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
