2d03468cf5cc7d4aefd281466e96c04fef25a8fe
Robert Domingo <robertdomingo@protonmail.com>
2021-06-05 11:28:50 -0400

    Add working button

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let Phrase = require("robdom-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+  } else {
     alert(`"${phrase.content}" is not a palindrome.`);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let form = document.querySelector("#palindromeTester");
+  form.addEventListener("submit", function() {
+    palindromeTester();
+  });
+});
+
