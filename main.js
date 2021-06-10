78f3c1317242a8a906e140729a634e1f00bce514
Robert Domingo <robertdomingo@protonmail.com>
2021-06-10 18:41:26 -0400

    Update npm module

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 let Phrase = require("robdom-palindrome");
 
-function palindromeTester() {
-  let string = prompt("Please enter a string for palindrome testing:");
-  let phrase = new Phrase(string);
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
   let palindromeResult = document.querySelector("#palindromeResult");
 
   if (phrase.palindrome()) {
-    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
   } else {
-    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is not a palindrome.`;
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  let button = document.querySelector("#palindromeTester");
-  button.addEventListener("click", function() {
-    palindromeTester();
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
   });
 });
 
