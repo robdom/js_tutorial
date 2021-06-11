32203c6616fd3c90eeb2f9ccb83f1cfc0e483d72
Robert Domingo <robertdomingo@protonmail.com>
2021-06-11 11:09:14 -0400

    Update npm module version

--- bundle.js
+++ bundle.js
@@ -43,12 +43,17 @@ function Phrase(content) {
   // For example:
   //   new Phrase("Hello, world!").letters() === "Helloworld"
   this.letters = function letters() {
-    return (this.content.match(/[a-z]/gi) || []).join("");
+    const lettersRegEx = /[a-z]/gi
+    return (this.content.match(lettersRegEx) || []).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.processedContent()) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
 }
 
